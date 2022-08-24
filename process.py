c9646a2d1645522421a1d319097bc7a99149a8de
Alraku <ekoalraku@gmail.com>
2022-08-24 18:46:23 +0200

    overwritten Process class init commit

---
+++ process.py
@@ -0,0 +1,24 @@
+import traceback
+import multiprocessing
+
+
+class Process(multiprocessing.Process):
+
+    def __init__(self, *args, **kwargs):
+        multiprocessing.Process.__init__(self, *args, **kwargs)
+        self._parent_conn, self._child_conn = multiprocessing.Pipe()
+        self._exception = None
+
+    def run(self):
+        try:
+            multiprocessing.Process.run(self)
+            self._child_conn.send(None)
+        except Exception as e:
+            tb = traceback.format_exc()
+            self._child_conn.send((e, tb))
+
+    @property
+    def exception(self):
+        if self._parent_conn.poll():
+            self._exception = self._parent_conn.recv()
+        return self._exception
