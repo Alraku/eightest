f59df936136c207b0e27fa8e15ba694f00fab8be
Alraku <ekoalraku@gmail.com>
2022-09-05 21:21:59 +0200

    Added logger to process creation

--- source/process.py
+++ source/process.py
@@ -1,18 +1,23 @@
 import traceback
 import multiprocess
+from logger import NewLogger
 
 
 class Process(multiprocess.Process):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, test_name, *args, **kwargs):
         multiprocess.Process.__init__(self, *args, **kwargs)
         self._parent_conn, self._child_conn = multiprocess.Pipe()
         self._exception = None
+        self._test_name = test_name
 
     def run(self):
         try:
+            logger = NewLogger(test_name=self._test_name).get_logger()
+            logger.info('EXECUTION OF %s HAS STARTED.', self._test_name)
             multiprocess.Process.run(self)
             self._child_conn.send(None)
+
         except Exception as e:
             tb = traceback.format_exc()
             self._child_conn.send((e, tb))
