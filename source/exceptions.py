523b8b14c543a303be98b5c72e7dcd849b4831bc
Alraku <ekoalraku@gmail.com>
2022-09-20 16:18:55 +0200

    Redefined exceptions to errors

--- source/exceptions.py
+++ source/exceptions.py
@@ -2,28 +2,32 @@ class Error(Exception):
     """
     Base class for other exceptions.
     """
-    pass
+    def __init__(self) -> None:
+        self.message = 'Base Custom Error Class'
+
+    def __str__(self) -> str:
+        return self.message
 
 
-class EmptyFileException(Error):
+class EmptyFileError(Error):
     """
     Raised when the opened file is empty.
     """
     def __init__(self, file_name) -> None:
         self.message = f"'{file_name}' file is empty."
-        self.file_name = file_name
-
-    def __str__(self) -> str:
-        return self.message
 
 
-class TestOutOfClassException(Error):
+class NoTestsFoundError(Error):
+    """
+    Raised when no tests were found.
+    """
+    def __init__(self) -> None:
+        self.message = "No tests found in the test folder."
+
+
+class TestOutOfClassError(Error):
     """
     Raised when test function was discovered out of Test Class.
     """
     def __init__(self, test_name) -> None:
         self.message = f"'{test_name}' test function out of Test Class."
-        self.test_name = test_name
-
-    def __str__(self) -> str:
-        return self.message
