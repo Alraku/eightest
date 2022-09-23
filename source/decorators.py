f7f37a32857ce3c616ac6cd4cc3411f31f920f4f
Alraku <ekoalraku@gmail.com>
2022-09-23 17:28:08 +0200

    Improved decorator template to class

--- source/decorators.py
+++ source/decorators.py
@@ -1,27 +1,58 @@
-from functools import wraps
+import inspect
+import functools
 
-dec_funcs = []
-
-
-def template(function):
-    @wraps(function)
-    def nested(*args, **kwargs):
-        return function(*args, **kwargs)
-
-    dec_funcs.append(function)
-    return nested
+from typing import Any, Callable
 
 
-REGRESSION_TEST = template
-SMOKE_TEST = template
+class Template(object):
+    """
+    Template for specific decorator creation.
+    """
+    def __init__(self, object) -> None:
+
+        self.object = object
+        functools.update_wrapper(self, object)
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Callable:
+        """
+        When decorator is being called check if is used
+        on whole class or single method. If class then
+        decorate all methods, otherwise function.
+
+        Returns:
+            Callable: Decorated function or method.
+        """
+        if inspect.isclass(self.object):
+            for name, method in inspect.getmembers(self.object):
+                if not inspect.isfunction(method) \
+                   or inspect.isbuiltin(method):
+                    continue
+
+                if not inspect.ismethod(method) and \
+                   not name.startswith('test_'):
+                    continue
+
+                setattr(self.object, name, self.decorate_method(method))
+            return self.object(*args, **kwargs)
+
+        else:
+            self.object(*args, **kwargs)
+
+    def decorate_method(self, original_func: Callable) -> Callable:
+        """
+        Helper function for decorating particular class method.
+
+        Args:
+            original_func (Callable): _description_
+
+        Returns:
+            Callable: Decorated method.
+        """
+        @functools.wraps(original_func)
+        def decorator(*args, **kwargs):
+            return original_func(self, *args, **kwargs)
+        return decorator
 
 
-# def decorate(function):
-#     @wraps(function)
-#     def inner():
-#         print("Some stuff 1")
-#         function()
-#         print("Some stuff 2")
-
-#     dec_funcs.append(function)
-#     return inner
+SMOKE_TEST = Template
+REGRESSION_TEST = Template
