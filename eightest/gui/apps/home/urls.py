750eb17c9d7d13b580c7ffbd9a36ce893fbc43c1
Alraku <ekoalraku@gmail.com>
2022-10-04 21:14:21 +0200

    Added navbar template along with styling

--- eightest/gui/apps/home/urls.py
+++ eightest/gui/apps/home/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
 from . import views
+from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('', views.home, name="home")
 ]
+urlpatterns += staticfiles_urlpatterns()
