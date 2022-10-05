1383d169aefd6a202872fc3c0b1c2e6334b61b45
Alraku <ekoalraku@gmail.com>
2022-10-05 19:05:06 +0200

    Added apps

--- eightest/gui/settings.py
+++ eightest/gui/settings.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'eightest.gui.apps'
+    'eightest.gui.apps',
+    "django_static_fontawesome"
 ]
 
 MIDDLEWARE = [
--- eightest/gui/urls.py
+++ eightest/gui/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gui.apps.home.urls'))
+    path('', include('gui.apps.home.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
