6ba46e6537ff12a8bda9eb832a8a9e7dfbf88327
Alraku <ekoalraku@gmail.com>
2022-10-12 11:54:46 +0200

    New tests added

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -1,11 +1,55 @@
-import time
+from time import sleep
+from eightest import TestCase, eLogger
 
-from eightest import TestCase
+log = eLogger.get_logger()
 
 
 class TestStrings(TestCase):
 
-    def test_upper(self):
-        text = 'text'
-        time.sleep(10)
-        assert text.upper() == 'TEXT'
+    def before(self) -> None:
+        log.debug('Before Test')
+        self.text = 'text'
+
+    def test_upper(self) -> None:
+        sleep(6)
+        assert self.text.upper() == 'TEXT'
+
+    def test_upper2(self) -> None:
+        sleep(6)
+        assert self.text.upper() == 'TEXT'
+
+    def test_upper3(self) -> None:
+        sleep(6)
+        assert self.text.upper() == 'TEXT'
+
+    def test_upper4(self) -> None:
+        sleep(6)
+        assert self.text.upper() == 'TEXT'
+
+    def test_upper5(self) -> None:
+        sleep(6)
+        assert self.text.upper() == 'TEXT'
+
+    def test_upper6(self) -> None:
+        sleep(6)
+        assert self.text.upper() == 'TEXT'
+
+    def test_upper7(self) -> None:
+        sleep(6)
+        assert self.text.upper() == 'TEXT'
+
+    # def test_capitalize(self) -> None:
+    #     sleep(2)
+    #     assert self.text.capitalize() == 'Text'
+
+    # def test_center(self) -> None:
+    #     sleep(4)
+    #     assert self.text.center(6) == ' text '
+
+    # def test_endswith(self) -> None:
+    #     sleep(6)
+    #     assert self.text.endswith('xt')
+
+    # def test_isalpha(self) -> None:
+    #     sleep(1)
+    #     assert self.text.isalpha()
--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,29 +1,29 @@
 import time
 
-from eightest import S_Logger, TestCase
+from eightest import eLogger, TestCase
 
-logger = S_Logger.get_logger()
+log = eLogger.get_logger()
 
 
 class TestSuite(TestCase):
 
     def before(self):
-        logger.debug('BEFORE LOG')
+        log.debug('Before Test')
 
     def after(self):
-        logger.debug('AFTER LOG')
+        log.debug('After Test')
 
-    def test_one(self):
-        logger.debug('TEST_ONE LOG')
-        time.sleep(1)
-        assert 0 == 0
+    # def test_one(self):
+    #     log.debug('TEST_ONE LOG')
+    #     time.sleep(1)
+    #     assert 0 == 0
 
-    def test_two(self):
-        logger.debug('TEST_TWO LOG')
-        time.sleep(2)
-        assert 1 == 0
+    # def test_two(self):
+    #     log.debug('TEST_TWO LOG')
+    #     time.sleep(2)
+    #     assert 1 == 0
 
-    def test_three(self):
-        logger.debug('TEST_THREE LOG')
-        1/0
-        assert 0 == 0
+    # def test_three(self):
+    #     log.debug('TEST_THREE LOG')
+    #     1/0
+    #     assert 0 == 0
