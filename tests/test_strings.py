7f6738f9239cbe896d008c21ab47f0f91a145374
Alraku <ekoalraku@gmail.com>
2022-10-16 14:58:58 +0200

    Added more test cases

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -11,45 +11,34 @@ class TestStrings(TestCase):
         self.text = 'text'
 
     def test_upper(self) -> None:
-        sleep(6)
+        sleep(7)
         assert self.text.upper() == 'TEXT'
 
-    def test_upper2(self) -> None:
+    def test_capitalize(self) -> None:
+        sleep(2)
+        assert self.text.capitalize() == 'Text'
+
+    def test_center(self) -> None:
+        sleep(4)
+        assert self.text.center(6) == ' text '
+
+    def test_endswith(self) -> None:
         sleep(6)
-        assert self.text.upper() == 'TEXT'
+        assert self.text.endswith('xt')
 
-    def test_upper3(self) -> None:
+    def test_isalpha(self) -> None:
+        sleep(1)
+        assert self.text.isalpha()
+
+    def test_center2(self) -> None:
+        sleep(1)
+        1/0
+        assert self.text.center(6) == ' text '
+
+    def test_endswith2(self) -> None:
         sleep(6)
-        assert self.text.upper() == 'TEXT'
+        assert self.text.endswith('xt')
 
-    def test_upper4(self) -> None:
-        sleep(6)
-        assert self.text.upper() == 'TEXT'
-
-    def test_upper5(self) -> None:
-        sleep(6)
-        assert self.text.upper() == 'TEXT'
-
-    def test_upper6(self) -> None:
-        sleep(6)
-        assert self.text.upper() == 'TEXT'
-
-    def test_upper7(self) -> None:
-        sleep(6)
-        assert self.text.upper() == 'TEXT'
-
-    # def test_capitalize(self) -> None:
-    #     sleep(2)
-    #     assert self.text.capitalize() == 'Text'
-
-    # def test_center(self) -> None:
-    #     sleep(4)
-    #     assert self.text.center(6) == ' text '
-
-    # def test_endswith(self) -> None:
-    #     sleep(6)
-    #     assert self.text.endswith('xt')
-
-    # def test_isalpha(self) -> None:
-    #     sleep(1)
-    #     assert self.text.isalpha()
+    def test_isalpha2(self) -> None:
+        sleep(1)
+        assert self.text.isdecimal()
