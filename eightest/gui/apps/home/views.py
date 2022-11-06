511f76b18d1790bdfe28917c63055a63dc1207a8
Alraku <ekoalraku@gmail.com>
2022-11-06 17:28:44 +0100

    Debugging stuff

--- eightest/gui/apps/home/views.py
+++ eightest/gui/apps/home/views.py
@@ -4,6 +4,7 @@ from runner import Runner
 from utilities import ROOT_DIR
 from django.shortcuts import render
 from django.http.response import JsonResponse
+from gui.apps.navbar.views import function
 
 os.chdir(ROOT_DIR)
 runner = Runner()
@@ -16,6 +17,7 @@ def home(request):
 
 def checkboxes(request):
     lista = request.POST.getlist('checks[]')
-    # request.session['my_data'] = lista
+    print(lista[0].test_name)
+    request.session['my_data'] = lista
     # return render(request, 'home.html')
-    return redirect
+    return function(request, {"xd": lista})
