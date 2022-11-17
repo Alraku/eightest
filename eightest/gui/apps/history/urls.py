ffef87632acec731c15c20980ad0cd5646d1ea61
Alraku <ekoalraku@gmail.com>
2022-11-17 12:40:44 +0100

    Resolving problems with showing log history

--- eightest/gui/apps/history/urls.py
+++ eightest/gui/apps/history/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('history', views.HistoryView.as_view(), name="history")
+    path('history', views.HistoryView.as_view(), name="history"),
+    path('get_response_hist', views.HistoryView.answer_me_hist, name="get_response_hist")
 ]
 urlpatterns += staticfiles_urlpatterns()
